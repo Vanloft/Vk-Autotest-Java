b9257b8f23882583f2a56e1b4497c30171e16ae2
Bulgakovwww <98288027+Vanloft@users.noreply.github.com>
2024-03-28 18:39:02 +0300

    Revert "Homework-1"

--- Task_1/src/test/java/vk/edu/page/MusicPage.java
+++
@@ -1,9 +0,0 @@
-package vk.edu.page;
-
-import static com.codeborne.selenide.Selenide.$x;
-
-public class MusicPage {
-    public MusicPage(){
-        $x("//*[@id='music_toolbar_button']").click();
-    }
-}
