d74c9ab7162d38951a193ea24d8de88df95ea59b
bulgakovwww <inbulgakov@gmail.com>
2024-03-28 18:35:01 +0300

    Убрал Мавен

---
+++ Task_0/src/org/example/TestTrue.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+public class TestTrue {
+    @Test
+    public void SimpleTest() {
+        assertTrue(true);
+    }
+}
