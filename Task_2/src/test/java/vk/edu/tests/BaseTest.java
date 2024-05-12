600746e586aedb7507d80550397be825c3ad076f
bulgakovwww <inbulgakov@gmail.com>
2024-05-12 22:51:56 +0300

    Gradlew

--- Task_2/src/test/java/vk/edu/tests/BaseTest.java
+++ Task_2/src/test/java/vk/edu/tests/BaseTest.java
@@ -1,46 +1,44 @@
 package vk.edu.tests;
 
-import com.codeborne.selenide.WebDriverRunner;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import vk.edu.elements.HeaderElement;
 import vk.edu.page.LoginPage;
 
-
 abstract public class BaseTest {
-    private final static String BASE_URL = "https://ok.ru/";
-    protected final static LoginPage loginPage = new LoginPage(BASE_URL);
+    private static final String BASE_URL = "https://ok.ru/";
+    protected static final LoginPage loginPage = new LoginPage(BASE_URL);
 
-    static {
+    @BeforeAll
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-    }
 
-    @BeforeAll
-    public static void setUp() {
-        LoginPage logPage = new LoginPage(BASE_URL);
         Configuration.baseUrl = BASE_URL;
         Configuration.browser = "chrome";
+        Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
 
+        LoginPage logPage = new LoginPage(BASE_URL);
     }
 
     @AfterEach
-    public void goToLoginPage(){
-        if (!loginPage.unsuccessLogin()){
+    public void goToLoginPage() {
+        if (!loginPage.unsuccessLogin()) {
             HeaderElement header = new HeaderElement();
             header.logOut();
         }
     }
+
     @AfterAll
     public static void tearDown() {
         Selenide.closeWebDriver();
